1c4b3b97b5c3e69dc2edd3267d802cc178b303e3
Brandon Lamb <blamb888@gmail.com>
2021-03-25 14:38:31 +0900

    change searchbar to stateful component

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ""
+    };
+  }
+
   handleChange = (event) => {
+    // Change the state of term
+    this.setState({
+      term: event.currentTarget.value
+    });
     const { fetchGiphy } = this.props;
     fetchGiphy(event.currentTarget.value);
   }
 
   render() {
     return (
-      <input className="form-search form-control" type="text" placeholder="Search for a gif" onChange={this.handleChange} />
+      <input
+        value={this.state.term}
+        className="form-search form-control"
+        type="text"
+        placeholder="Search for a gif"
+        onChange={this.handleChange}
+      />
     );
   }
 }
