9435ef5ccc1c060a60e50d86c825c64a7099a33f
Brandon Lamb <blamb888@gmail.com>
2021-04-05 20:58:56 +0900

    try background images with urls

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -37,10 +37,10 @@ class App extends Component {
   render() {
     const { gifIdSelected, gifIdList } = this.state;
     const styleObj = {
-      backgroundImage: 'url(./images/background-large.png)'
+      backgroundImage: 'url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/29541bea-9f34-4505-98e3-220fb1a66afa/ddbynxg-7baf2ac6-c0b6-40c5-bec2-6102d37e3611.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMjk1NDFiZWEtOWYzNC00NTA1LTk4ZTMtMjIwZmIxYTY2YWZhXC9kZGJ5bnhnLTdiYWYyYWM2LWMwYjYtNDBjNS1iZWMyLTYxMDJkMzdlMzYxMS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.aHMmRcRk0tdik6o43A9i23nDr3IDRpSulrMdORKb5hw)'
     };
     const styleObj2 = {
-      backgroundImage: 'url(./images/right-side2.jpg)'
+      backgroundImage: 'url(https://wallpaperaccess.com/full/825441.jpg)'
     };
     return (
       <div>
